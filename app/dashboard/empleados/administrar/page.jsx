026eb4f4a2611981e59c2321857b750b95b05265
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-12 17:20:09 -0600

    chore: Update imports in page.jsx

--- app/dashboard/empleados/administrar/page.jsx
+++ app/dashboard/empleados/administrar/page.jsx
@@ -1,9 +1,9 @@
-// import DashboardEmployedAdmin from '@/components/component/dashboard-employed-admin';
+import DashboardEmployedAdmin from '@/components/component/dashboard-employed-admin';
 
 const AddEmployed = () => {
   return (
     <>
-      {/* <DashboardEmployedAdmin /> */}
+      <DashboardEmployedAdmin />
     </>
   )
 }
