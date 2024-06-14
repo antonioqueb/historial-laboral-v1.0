2058756ed585e56648bb5b49b8fe9a99de3c28e3
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-14 11:53:15 -0600

    chore: Update federated-logout route to add Cache-Control header

--- components/ResponseSearch.jsx
+++ components/ResponseSearch.jsx
@@ -1,9 +1,5 @@
-
-import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar"
-import Link from "next/link"
-
-
-
+import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
+import Link from "next/link";
 
 const data = [
   {
@@ -26,34 +22,29 @@ const data = [
     nombre: "Shu Ding",
     nss: "DINS880501JKL"
   },
-]
+];
 
-export default function Component(data) {
+export default function Component() {
   return (
-    <div className="flex items-center justify-center  bg-zinc-100 dark:bg-zinc-900 ">
+    <div className="flex items-center justify-center bg-zinc-100 dark:bg-zinc-900">
       <section className="p-6 max-w-7xl w-full py-8 bg-white dark:bg-zinc-800 shadow-lg rounded-lg">
         <div className="grid gap-6">
-
-       {data.map((iconText, nombre, nss) => (
-          <Link href="/empleado">
-          <div className="p-2 flex cursor-pointer items-center gap-4  hover:bg-zinc-200  dark:hover:bg-zinc-900  rounded-lg ">
-         
-            <Avatar className="w-12 h-12">
-              <AvatarImage alt="@jaredpalmer" src="/placeholder-avatar.jpg" />
-              <AvatarFallback>{iconText}</AvatarFallback>
-            </Avatar>
-            <div className="grid gap-1">
-              <h3 className="font-medium">{nombre}</h3>
-              <p className="text-sm text-zinc-600 dark:text-zinc-400">{nss}</p>
-            </div>
-           
-          </div>
-          </Link>
-        ))
-      } 
-          
+          {data.map(({ iconText, nombre, nss }) => (
+            <Link href="/empleado" key={nss}>
+              <div className="p-2 flex cursor-pointer items-center gap-4 hover:bg-zinc-200 dark:hover:bg-zinc-900 rounded-lg">
+                <Avatar className="w-12 h-12">
+                  <AvatarImage alt="@jaredpalmer" src="/placeholder-avatar.jpg" />
+                  <AvatarFallback>{iconText}</AvatarFallback>
+                </Avatar>
+                <div className="grid gap-1">
+                  <h3 className="font-medium">{nombre}</h3>
+                  <p className="text-sm text-zinc-600 dark:text-zinc-400">{nss}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
       </section>
     </div>
-  )
+  );
 }
