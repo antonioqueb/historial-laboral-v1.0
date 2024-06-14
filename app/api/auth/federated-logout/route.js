65d7e742e50b572b30fc4868dda3ad12604ea579
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-14 11:26:37 -0600

    chore: Update federated-logout route to add Cache-Control header

--- app/api/auth/federated-logout/route.js
+++ app/api/auth/federated-logout/route.js
@@ -1,44 +1,20 @@
-import { NextResponse } from 'next/server';
-import { getToken } from 'next-auth/jwt';
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  async headers() {
+    return [
+      {
+        // Asegúrate de que la ruta API no se almacene en caché
+        source: '/api/auth/federated-logout',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-function logoutParams(token) {
-  return {
-    id_token_hint: token.idToken,
-    post_logout_redirect_uri: process.env.NEXTAUTH_URL,
-  };
-}
-
-function handleEmptyToken() {
-  const response = { error: "No session present" };
-  const responseHeaders = { status: 400 };
-  return NextResponse.json(response, responseHeaders);
-}
-
-function sendEndSessionEndpointToURL(token) {
-  const endSessionEndPoint = new URL(
-    `${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/logout`
-  );
-  const params = logoutParams(token);
-  const endSessionParams = new URLSearchParams(params);
-  const response = { url: `${endSessionEndPoint.href}/?${endSessionParams}` };
-  return NextResponse.json(response);
-}
-
-export async function GET(req) {
-  try {
-    const token = await getToken({ req });
-    if (token) {
-      return sendEndSessionEndpointToURL(token);
-    }
-    return handleEmptyToken();
-  } catch (error) {
-    console.error(error);
-    const response = {
-      error: "Unable to logout from the session",
-    };
-    const responseHeaders = {
-      status: 500,
-    };
-    return NextResponse.json(response, responseHeaders);
-  }
-}
+export default nextConfig;
