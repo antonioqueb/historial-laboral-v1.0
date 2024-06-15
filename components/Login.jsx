7ea8dea257a1443b1439538e09f5a49d6ac38180
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-15 17:40:42 -0600

    feat: Actualizar Login.jsx para iniciar sesión con Keycloak

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useSession, signIn } from 'next-auth/react';
@@ -15,15 +16,15 @@ export default function Login() {
   }, [status, router]);
 
   return (
-    <div className="flex justify-center items-center h-screen w-full bg-gray-100">
+    <div className="flex justify-center items-center h-screen w-full ">
       {status === 'loading' ? (
         <p>Loading...</p>
       ) : (
         <Button 
           onClick={() => signIn("keycloak")} 
-          className="px-6 py-2 bg-primary text-black rounded-md shadow-md hover:bg-primary/90"
+          className="px-6 py-4 bg-primary text-black rounded-md shadow-md hover:bg-primary/90"
         >
-          Iniciar Sesión Keycloak
+          Iniciar Sesión con Keycloak
         </Button>
       )}
     </div>
