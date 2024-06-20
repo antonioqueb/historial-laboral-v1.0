db1fba218ab095f6e692a726ec2b074cbd45dca9
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-06-20 07:34:05 -0600

    feat: Remove unused import in test-session route

--- app/api/test-session/route.js
+++ app/api/test-session/route.js
@@ -1,4 +1,3 @@
-// app/api/test-session/route.js
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../auth/[...nextauth]/route';
 import { NextResponse } from 'next/server';
